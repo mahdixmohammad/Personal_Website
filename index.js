c5162f4caa84e8293a684796e615e22c4efeadcb
mahdixmohammad <mahdi3mohammad@gmail.com>
2024-06-23 23:58:40 -0400

    Added pop in transitions to sections, added 'use strict' in javascript file, and overhauled css file with SASS.

--- index.js
+++ index.js
@@ -1,6 +1,63 @@
-// when user clicks on the hamburger icon (small screen sizes only)
-hamburger = document.querySelector("#hamburger");
+"use strict"
+
+// DOM variables
+const nav = document.querySelector("nav");
+const hamburger = document.querySelector("#hamburger");
+
+// enables dropdown menu with nav links when hamburger is clicked (small screen sizes only)
 hamburger.onclick = function() {
-    nav = document.querySelector("nav");
     nav.classList.toggle("active");
 }
+
+// remove active nav when user clicks nav link
+const navlinks = document.querySelectorAll("nav a, #contactbtn");
+navlinks.forEach(navlink => {
+    navlink.onclick = function() {
+        let element = document.getElementById(navlink.textContent.toLowerCase());
+
+        if (navlink.id == "contactbtn") element = document.getElementById("contact");
+
+        let offset = element.clientHeight * 0.4;
+
+        if (element.classList.contains("visible")) offset = 20;
+
+        let elementPosition = element.getBoundingClientRect().top;
+        let offsetPosition = elementPosition + window.scrollY - offset;
+    
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+
+        // if on smaller screen, remove active nav class
+        nav.classList.remove("active");
+    }
+});
+
+// as user scrolls down, sections pop in
+function popInAnimation() {
+    const reveals = document.querySelectorAll(".popin");
+    const windowHeight = window.innerHeight;
+    const elementVisible = 75;
+
+    reveals.forEach((element) => {
+        let elementTop = element.getBoundingClientRect().top;
+        let distanceFromView = Math.max(0, windowHeight - elementTop - elementVisible);
+        let opacity = (distanceFromView / elementVisible) * 0.4 - 1;
+        let scale = ((distanceFromView / elementVisible)) * 0.1 + 0.25;
+        
+        if (scale >= 1) {
+            scale = 1;
+            element.classList.add("visible");
+        } else {
+            element.classList.remove("visible");
+        }
+        element.style.opacity = opacity;
+        element.style.transform = `scale(${scale})`;
+    });
+}
+
+window.addEventListener("scroll", popInAnimation);
+
+// To check the scroll position on page load
+popInAnimation();
