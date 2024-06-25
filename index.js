8bd099a46194b01ada7631a0acde8e66c36cd9f7
mahdixmohammad <mahdi3mohammad@gmail.com>
2024-06-24 23:56:25 -0400

    Added projects section, and adjusted the pop in animations

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ navlinks.forEach(navlink => {
         let offset = element.clientHeight * 0.4;
 
         if (element.classList.contains("visible")) offset = 20;
+        
+        if (element.id == "projects") {
+            element = document.querySelector("#projects .title");
+            offset = 120;
+        }
 
+        console.log(offset)
         let elementPosition = element.getBoundingClientRect().top;
         let offsetPosition = elementPosition + window.scrollY - offset;
     
@@ -38,13 +44,16 @@ navlinks.forEach(navlink => {
 function popInAnimation() {
     const reveals = document.querySelectorAll(".popin");
     const windowHeight = window.innerHeight;
-    const elementVisible = 75;
 
     reveals.forEach((element) => {
+        let elementVisible = element.clientHeight * 0.11;
+        if (elementVisible < 40 || element.classList.contains("project")) elementVisible = 40;
+        else if (elementVisible > 80) elementVisible = 80;
         let elementTop = element.getBoundingClientRect().top;
         let distanceFromView = Math.max(0, windowHeight - elementTop - elementVisible);
         let opacity = (distanceFromView / elementVisible) * 0.4 - 1;
         let scale = ((distanceFromView / elementVisible)) * 0.1 + 0.25;
+
         
         if (scale >= 1) {
             scale = 1;
