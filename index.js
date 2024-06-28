1dd6b55c135185f8289d1385a3a86618cfcc7586
mahdixmohammad <mahdi3mohammad@gmail.com>
2024-06-28 17:56:34 -0400

    Remove redundant css and make code cleaner

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // DOM variables
 const nav = document.querySelector("nav");
 const hamburger = document.querySelector("#hamburger");
+const navlinks = document.querySelectorAll("nav a, #contactbtn");
+const reveals = document.querySelectorAll(".popin");
+const progressBars = document.querySelectorAll(".progress");
 
 // enables dropdown menu with nav links when hamburger is clicked (small screen sizes only)
 hamburger.onclick = function() {
@@ -9,7 +12,6 @@ hamburger.onclick = function() {
 }
 
 // remove active nav when user clicks nav link
-const navlinks = document.querySelectorAll("nav a, #contactbtn");
 navlinks.forEach(navlink => {
     navlink.onclick = function() {
         // if on smaller screen, remove active nav class
@@ -19,18 +21,14 @@ navlinks.forEach(navlink => {
 
 // as user scrolls down, sections pop in
 function popInAnimation() {
-    const reveals = document.querySelectorAll(".popin");
-    const windowHeight = window.innerHeight;
-
     reveals.forEach(element => {
         let elementVisible = element.clientHeight * 0.09;
         if (elementVisible < 30) elementVisible = 30;
         else if (elementVisible > 60) elementVisible = 60;
 
         if (element.classList.contains("project")) elementVisible = 20;
-        // elementVisible = 30;
         let elementTop = element.getBoundingClientRect().top;
-        let distanceFromView = Math.max(0, windowHeight - elementTop - elementVisible);
+        let distanceFromView = Math.max(0, window.innerHeight - elementTop - elementVisible);
         let opacity = (distanceFromView / elementVisible) * 0.4 - 1;
         let scale = ((distanceFromView / elementVisible)) * 0.1 + 0.25;
         
@@ -51,7 +49,6 @@ window.addEventListener("scroll", popInAnimation);
 popInAnimation();
 
 // loads the width of all the progress bars in skills section automatically
-const progressBars = document.querySelectorAll(".progress");
 progressBars.forEach(progressBar => {
     progressBar.querySelector("div").style.width = `${progressBar.textContent}`;
 })
