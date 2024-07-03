02db82adf12c0547f43160a4c56913b58742b776
mahdixmohammad <mahdi3mohammad@gmail.com>
2024-07-03 17:38:35 -0400

    Overhaul JavaScript with TypeScript, remove redundant comments, format code better

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'index.css',
-        }),
-    ],
-    output: {
-      filename: 'index.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                loader: "html-loader",
-            },        
-            {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
+	mode: "production",
+	entry: "./src/index.ts",
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "index.css",
+		}),
+	],
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.html$/,
+				loader: "html-loader",
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+		],
+	},
 };
